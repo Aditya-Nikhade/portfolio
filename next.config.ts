dc8377037d3ba6203ce3761d554c3a223ada514c
Aditya Nikhade <112113627+Aditya-Nikhade@users.noreply.github.com>
2025-05-26 13:02:29 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true, // ✅ Prevents ESLint errors from failing the build
+  },
 };
 
 export default nextConfig;
